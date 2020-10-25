ab1d8930c7d00d460ecb5a1e87a7245f67a973b9
Fen <48224242+learningbaby@users.noreply.github.com>
2020-10-25 23:39:16 +1300

    First refactor of the Test_1 and Test_0_NoInput

--- CNZBATests/CNZBATests/LoginTests.cs
+++ CNZBATests/CNZBATests/LoginTests.cs
@@ -11,45 +11,49 @@ namespace CNZBATests
     [TestCategory("LoginModule")]
     public class LoginTests
     {
+        IWebDriver driver;
+        LoginPage loginPage;
+
+        [TestInitialize]
+        public void SetupBeforEverySingleMethod()
+        {
+            driver = ChromeDriver();
+            loginPage = new LoginPage(driver);
+            loginPage.Open();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestCleanup]
+        public void CleanupAfterEverySingleMethod()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
         [TestMethod]
-        [Description("Validate that the user is able to login successfully using validate date")]
+        [Description("Validate that the user is able to login successfully using validate data")]
         public void Test_1()
         {
             var userInfo = new TestUser();
             userInfo.EmailAddress = "guest@guest.com";
             userInfo.PassWord = "q1111111";
 
-            var driver = ChromeDriver();
-            LoginPage loginPage = new LoginPage(driver);
-            loginPage.Open();
-            Thread.Sleep(1000);
-            driver.Manage().Window.Maximize();
-
             loginPage.InputUserInfoAndLogin(userInfo);
             Assert.AreEqual("CBA Invoicing", driver.Title);
-
-            driver.Close();
-            driver.Quit();
-
         }
+
         [TestMethod]
         [Description("Validate no input")]
         public void Test_0_NoInput()
         {
-            var driver = ChromeDriver();
-            LoginPage loginPage = new LoginPage(driver);
-            loginPage.Open();
-            Thread.Sleep(1000);
-            driver.Manage().Window.Maximize();
-
             loginPage.NoInput();
             Assert.IsTrue(loginPage.AlertforEmailAddress.Text.Contains("You Must Enter A Value."));
             Assert.IsTrue(loginPage.AlertforPassword.Text.Contains("You Must Enter A Value."));
             Assert.IsFalse(loginPage.LoginButton.Enabled);
 
-            driver.Close();
-            driver.Quit();
         }
+
+        
 
         private IWebDriver ChromeDriver()
         {
